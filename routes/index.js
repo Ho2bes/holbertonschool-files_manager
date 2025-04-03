81739a2d68db685b1ccf63c6ed647b8a1107d8d6
Ho2bes <8696@holbertonstudents.com>
2025-04-03 15:39:18 +0200

    fix routes

--- routes/index.js
+++ routes/index.js
@@ -9,11 +9,15 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/users', UsersController.postNew);
-router.post('/files', FilesController.postUpload);
 
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
+
+router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
+
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
 
 export default router;
